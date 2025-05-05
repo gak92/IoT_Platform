b33b204b8565087e19839fa8d7f74126d8765bb7
Ghazanfar Ali <gak_92@hotmail.com>
2025-05-05 21:16:21 +0300

    Test initial app is working

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
 # Main Flask App
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_mqtt import Mqtt
+import json
+
+# Initialize Falsk App
+app = Flask(__name__)
+
+
+@app.route('/')
+def test():
+    return "Testing .. Flask App is working..."
+
+if __name__ == '__main__':
+    app.run(debug=True)
