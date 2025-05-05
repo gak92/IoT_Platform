7b9b26621428259312dd769f43ea93c8035698f3
Ghazanfar Ali <gak_92@hotmail.com>
2025-05-05 15:27:18 +0300

    Adding basic configuration settings

--- config.py
+++ config.py
@@ -1,1 +1,6 @@
 # Configurations
+MQTT_BROKER_URL = "localhost"  # Change if using external MQTT broker
+MQTT_BROKER_PORT = 1883
+SECRET_KEY = "iotlabproject1"
+SQLALCHEMY_DATABASE_URI = "sqlite:///database.db"
+SQLALCHEMY_TRACK_MODIFICATIONS = False
