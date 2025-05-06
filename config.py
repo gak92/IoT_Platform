b4c02a8ef875f865dd18d2af6730032a711cb84d
Ghazanfar Ali <gak_92@hotmail.com>
2025-05-06 13:35:07 +0300

    Update Config.py file for MQTT settings

--- config.py
+++ config.py
@@ -1,6 +1,10 @@
 # Configurations
 MQTT_BROKER_URL = "localhost"  # Change if using external MQTT broker
 MQTT_BROKER_PORT = 1883
+MQTT_USERNAME = ''  # Set if needed
+MQTT_PASSWORD = ''  # Set if needed
+MQTT_KEEPALIVE = 60
+MQTT_TLS_ENABLED = False
 SECRET_KEY = "iotlabproject1"
 SQLALCHEMY_DATABASE_URI = "sqlite:///database.db"
 SQLALCHEMY_TRACK_MODIFICATIONS = False
